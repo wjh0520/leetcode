f3c2033aa01bc0adef77f8fdbaebc363330582a6
wjh <wjh@qq.com>
2021-04-27 10:59:47 +0800

    leetcode题333333333333

--- src/main/java/com/wjh/gitdemo/GitCommit2.java
+++ src/main/java/com/wjh/gitdemo/GitCommit2.java
@@ -3,5 +3,6 @@ package com.wjh.gitdemo;
 public class GitCommit2 {
     public static void main(String[] args) {
         System.out.println("2222222222222222222");
+        System.out.println("3333333333333333333");
     }
 }
