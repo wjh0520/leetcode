7b538a2eb31514994a16661c829ec7d8e878c04d
wjh <wjh@qq.com>
2021-04-27 11:10:40 +0800

    master  commit

--- src/main/java/com/wjh/gitdemo/GitCommit2.java
+++ src/main/java/com/wjh/gitdemo/GitCommit2.java
@@ -5,5 +5,6 @@ public class GitCommit2 {
         System.out.println("2222222222222222222");
         System.out.println("3333333333333333333");
         System.out.println("host-fix444444444444");
+        System.out.println("master test 55555555");
     }
 }
